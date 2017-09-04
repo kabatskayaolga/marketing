ef246eab15a8e1924426fe31be6c1cbceb883941
kabatskayaolga <kabatskaya.olga@gmail.com>
2017-09-04 15:25:31 +0300

    add changes 1.0001

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,4 +1,6 @@
 
+// Change Logo
+
 function changeLogo(){
 	if ($(window).width() < 700) {
 		$('.header_logo__image').attr('src', 'images/logo-mobile.png');
@@ -7,18 +9,38 @@ function changeLogo(){
 	}
 };
 
-$(document).ready(function(){console.log('ff');changeLogo();});
-$(window).resize(function(){console.log('ff');changeLogo();});
+// Menu
+
+function openMenu(){
+	if ($(window).width() > 1040) {
+		$('.header_nav').show();
+	} else{
+		$('.header_nav').hide();
+	}
+};
 
 $(document).ready(function(){
+
+	changeLogo();
+
+	// Menu
+
 	$('.open_menu').click(function(){
 		$('.header_nav').css('display', 'flex');
 		$(".header_nav").append("<a class='button button-close'>x</a>");
+		$("body").addClass('body-overflow');
 	});
 
 	$(".header_nav").on('click', '.button-close',function(){
-		console.log('gfdg');
+		
 		$('.header_nav').hide();
 		$(this).remove();
+		$("body").removeClass('body-overflow');
 	});
+});
+
+
+$(window).resize(function(){
+	changeLogo();
+	openMenu();
 });
